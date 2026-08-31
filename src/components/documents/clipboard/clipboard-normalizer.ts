135812e8c2773243e6a52190497866eb1f4a0acc
Jaystarbliss studios <82984404+Jaystarbliss-studios@users.noreply.github.com>
2026-08-31 17:28:56 +0100

    fix: remove remaining duplicate line-break normalizer

--- src/components/documents/clipboard/clipboard-normalizer.ts
+++ src/components/documents/clipboard/clipboard-normalizer.ts
@@ -65,22 +65,3 @@ function normalizeEscapedLineBreaks(root: HTMLElement) {
     textNode.parentNode?.replaceChild(fragment, textNode);
   });
 }
-function normalizeEscapedLineBreaks(root: HTMLElement) {
-  const walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
-  const nodes: Text[] = [];
-  let node: Node | null;
-  while ((node = walker.nextNode())) nodes.push(node as Text);
-
-  nodes.forEach((textNode) => {
-    const value = textNode.nodeValue || '';
-    if (!/(?:\r?\n|\\n|\r)/.test(value)) return;
-
-    const parts = value.split(/(?:\r?\n|\\n|\r)/g);
-    const fragment = document.createDocumentFragment();
-    parts.forEach((part, index) => {
-      if (part) fragment.appendChild(document.createTextNode(part));
-      if (index < parts.length - 1) fragment.appendChild(document.createElement('br'));
-    });
-    textNode.parentNode?.replaceChild(fragment, textNode);
-  });
-}
